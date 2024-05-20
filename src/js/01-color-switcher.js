b239c7f5cb6909a942267afad67a36f7ccab4b27
deioh <143625578+deioh@users.noreply.github.com>
2024-05-20 22:32:05 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const button_start = document.querySelector('button[data-start]');
+const button_stop = document.querySelector('button[data-stop]');
+
+let intervalId = null;
+
+button_start.addEventListener('click', () => {
+  button_start.setAttribute('disabled', true);
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+button_stop.addEventListener('click', () => {
+  button_start.removeAttribute('disabled');
+  clearInterval(intervalId);
+});
