9482898015ec281fa188f22bc661c897d96c1b81
deioh <gamekuma@gmail.com>
2024-05-24 16:36:51 +0800

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,23 +17,32 @@ function promiseGenerator(e) {
   let delayValue = Number(delay.value);
   // let stepValue = Number(step.value);
   // let amountValue = Number(amount.value);
-  // for (let i = 1; i <= amountValue; i+=1) {
-  //   createPromise(i, delayValue)
-  //     .then(({ position, delay }) => {
-  //       Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  //     })
-  //     .catch(({ position, delay }) => {
-  //       Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  //     });
-  //   delayValue += stepValue;
-  // }
+  for (let positionValue = 1; positionValue <= amount.value; positionValue++) {
+    createPromise(positionValue, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  delayValue += Number(step.value);
 }
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  setTimeout(() => {
+    if (shouldResolve) {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  }, delay);
+
+  //   if (shouldResolve) {
+  //     // Fulfill
+  //   } else {
+  //     // Reject
+  //   }
 }
